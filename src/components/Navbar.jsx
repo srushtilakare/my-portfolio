529111337f17d0f6138339a324f9165a8b2e4c28
Srushti Lakare <srushti1924@gmail.com>
2026-05-19 21:09:20 +0530

    responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import "../styles/navbar.css";
-
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
 
@@ -15,45 +17,56 @@ function Navbar() {
         Portfolio
       </Link>
 
-      <ul className="nav-links">
+      {/* HAMBURGER MENU */}
 
-        <li>
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* NAV LINKS */}
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/">
             Home
           </Link>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/skills">
             Skills
           </Link>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/projects">
             Projects
           </Link>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/research">
             Research
           </Link>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/certificates">
             Certificates
           </Link>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <Link to="/contact">
             Contact
           </Link>
         </li>
-
-        
 
       </ul>
 
